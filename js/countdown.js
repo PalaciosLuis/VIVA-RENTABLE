a810b90861bfc1a40af3ec0b50aacecd4c0a9778
Luis Javier <924333523javier@gmail.com>
2024-01-30 16:11:12 -0500

    update hour

--- js/countdown.js
+++ js/countdown.js
@@ -1,8 +1,8 @@
 simplyCountdown('#cuenta', {
 	year: 2024, // required
-	month: 1, // required
-	day: 28, // required
-	hours: 7, // Default is 0 [0-23] integer
+	month: 2, // required
+	day: 12, // required
+	hours: 19, // Default is 0 [0-23] integer
 	minutes: 0, // Default is 0 [0-59] integer
 	seconds: 0, // Default is 0 [0-59] integer
 	words: { //words displayed into the countdown
