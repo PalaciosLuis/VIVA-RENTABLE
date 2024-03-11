27a1862d47c095730f62f41fb7b325be9cbcbe0c
Luis Javier <924333523javier@gmail.com>
2024-03-11 14:16:16 -0500

    Update main.js and add new image files

--- js/main.js
+++ js/main.js
@@ -1,38 +1,41 @@
-// Fecha y hora del webinar (Año, Mes (0-11), Día, Hora, Minuto, Segundo)
-const webinarDate = new Date(2024, 2, 16, 19, 0, 0); // 1 de Marzo de 2024 a las 10:00 AM
+// // Fecha y hora del webinar (Año, Mes (0-11), Día, Hora, Minuto, Segundo)
+// const webinarDate = new Date(2024, 2, 16, 19, 0, 0); // 1 de Marzo de 2024 a las 10:00 AM
 
-// Función para actualizar el contador
-function updateCountdown() {
-  const now = new Date().getTime();
-  const distance = webinarDate - now;
+// // Función para actualizar el contador
+// function updateCountdown() {
+//   const now = new Date().getTime();
+//   const distance = webinarDate - now;
 
-  // Cálculos para días, horas, minutos y segundos
-  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+//   // Cálculos para días, horas, minutos y segundos
+//   const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//   const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Mostrar el contador en la página
-  document.getElementById("countdown").innerHTML = `
-           <div>${days} </div>
-           <div>${hours} </div>
-           <div>${minutes} </div>
-           <div>${seconds} </div>
-       `;
+//   // Mostrar el contador en la página
+//   document.getElementById("countdown").innerHTML = `
+//            <div>${days} </div>
+//            <div>${hours} </div>
+//            <div>${minutes} </div>
+//            <div>${seconds} </div>
+//        `;
 
-  // Si la cuenta regresiva ha terminado, mostrar un mensaje
-  if (distance < 0) {
-    clearInterval(timer);
-    document.getElementById("countdown").innerHTML = "¡El webinar ha comenzado!";
-  }
-}
+//   // Si la cuenta regresiva ha terminado, mostrar un mensaje
+//   if (distance < 0) {
+//     clearInterval(timer);
+//     document.getElementById("countdown").innerHTML = "¡El webinar ha comenzado!";
+//   }
+// }
 
-// Actualizar el contador cada segundo
-const timer = setInterval(updateCountdown, 1000);
+// // Actualizar el contador cada segundo
+// const timer = setInterval(updateCountdown, 1000);
 
-// Actualizar el contador de inmediato para evitar el retraso inicial
-updateCountdown();
+// // Actualizar el contador de inmediato para evitar el retraso inicial
+// updateCountdown();
 
+
+
+start()
 
 const accordionItems = document.querySelectorAll('.accordion-item');
 
